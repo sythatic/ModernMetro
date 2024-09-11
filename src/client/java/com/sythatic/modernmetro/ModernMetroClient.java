8cfe9ac0b31f8f875007eb92de86e6fc8865983f
Dom Panaia <dompanaia@gmail.com>
2024-09-10 21:55:59 -0400

    Update 1.0.1
    Add support for emissive rails

--- src/client/java/com/sythatic/modernmetro/ModernMetroClient.java
+++ src/client/java/com/sythatic/modernmetro/ModernMetroClient.java
@@ -4,11 +4,19 @@ import com.sythatic.modernmetro.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
+import net.minecraft.text.Text;
 
 
 public class ModernMetroClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        FabricLoader.getInstance().getModContainer("modernmetro").ifPresent(container -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of("shader-support", "shader-support"), container, Text.literal("Emissive Rails"), ResourcePackActivationType.NORMAL);
+        });
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.POWERRAIL1, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.POWERRAIL2, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.POWERRAIL3, RenderLayer.getCutout());
