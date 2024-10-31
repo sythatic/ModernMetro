5932ef738e80a3885e7272341ba648e94bc32efc
Dom Panaia <dompanaia@gmail.com>
2024-10-31 18:49:14 -0400

    1.0.4-pre1

--- src/main/java/com/sythatic/modernmetro/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/com/sythatic/modernmetro/mixin/AbstractMinecartEntityMixin.java
@@ -3,6 +3,7 @@ package com.sythatic.modernmetro.mixin;
 import com.sythatic.modernmetro.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
@@ -16,19 +17,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(AbstractMinecartEntity.class)
 public abstract class AbstractMinecartEntityMixin extends Entity {
 	@Unique
-	private double maxSpeed = 8.0;
+	private double maxSpeed = 2.0;
 
 	public AbstractMinecartEntityMixin(EntityType<?> entityType, World world) {
 		super(entityType, world);
-	}
-
-	private double getSpeedForRail(BlockState blockState) {
-		if (blockState.isOf(ModBlocks.POWERRAIL1)) return 8.0;
-		if (blockState.isOf(ModBlocks.POWERRAIL2)) return 20.0;
-		if (blockState.isOf(ModBlocks.POWERRAIL3)) return 40.0;
-		if (blockState.isOf(ModBlocks.POWERRAIL4)) return 80.0;
-		if (blockState.isOf(ModBlocks.POWERRAIL5)) return 120.0;
-		return 6.0;
 	}
 
 	@Redirect(method = "moveOnRail", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
@@ -40,18 +32,43 @@ public abstract class AbstractMinecartEntityMixin extends Entity {
 	private Vec3d increaseAccelForNewRails(Vec3d vec, double x, double y, double z) {
 		Vec3d newvec = vec.add(x, y, z);
 		BlockState blockState = this.getWorld().getBlockState(this.getBlockPos());
-		return newvec.multiply(getSpeedForRail(blockState) / 8d);
+		if (blockState.isOf(ModBlocks.POWERRAIL1)) {
+			return newvec.multiply(8 / 8d);
+		} else if (blockState.isOf(ModBlocks.POWERRAIL2)) {
+			return newvec.multiply(16 / 8d);
+		} else if (blockState.isOf(ModBlocks.POWERRAIL3)) {
+			return newvec.multiply(32 / 8d);
+		} else if (blockState.isOf(ModBlocks.POWERRAIL4)) {
+			return newvec.multiply(64 / 8d);
+		} else if (blockState.isOf(ModBlocks.POWERRAIL5)) {
+			return newvec.multiply(128d / 8d);
+		}
+		return newvec;
 	}
 
 	@Redirect(method = "moveOnRail", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(DD)D"))
 	private double increaseSpeedCap(double a, double b) {
-		return Math.min(8.0, b);
+		return Math.min(2.0, b);
 	}
 
 	@Redirect(method = "moveOnRail", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;getMaxSpeed()D"))
 	public double increaseMaxSpeedOnNewRails(AbstractMinecartEntity instance) {
+		double speed = maxSpeed;
 		BlockState blockState = this.getWorld().getBlockState(this.getBlockPos());
-		maxSpeed = getSpeedForRail(blockState);
-		return maxSpeed / (this.isTouchingWater() ? 40.0 : 20.0);
+		if (blockState.isOf(Blocks.POWERED_RAIL)) {
+			speed = 2.0;
+		} else if (blockState.isOf(ModBlocks.POWERRAIL1)) {
+			speed = 8.0;
+		} else if (blockState.isOf(ModBlocks.POWERRAIL2)) {
+			speed = 16.0;
+		} else if (blockState.isOf(ModBlocks.POWERRAIL3)) {
+			speed = 32.0;
+		} else if (blockState.isOf(ModBlocks.POWERRAIL4)) {
+			speed = 64.0;
+		} else if (blockState.isOf(ModBlocks.POWERRAIL5)) {
+			speed = 128.0;
+		}
+		maxSpeed = speed;
+		return speed / (this.isTouchingWater() ? 16.0 : 8.0);
 	}
 }
