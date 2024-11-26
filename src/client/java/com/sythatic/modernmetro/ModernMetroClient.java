b1951472b13c979c48aef9e8db69bc023def8faf
Dom Panaia <dompanaia@gmail.com>
2024-11-26 16:04:24 -0500

    1.1.2-pre2

--- src/client/java/com/sythatic/modernmetro/ModernMetroClient.java
+++ src/client/java/com/sythatic/modernmetro/ModernMetroClient.java
@@ -15,7 +15,7 @@ public class ModernMetroClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         FabricLoader.getInstance().getModContainer("modernmetro").ifPresent(container -> {
-            ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of("emissive-rails", "emissive-rails"), container, Text.literal("Emissive Rails"), ResourcePackActivationType.NORMAL);
+            ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of("shader-support", "shader-support"), container, Text.literal("Shader Support"), ResourcePackActivationType.NORMAL);
         });
         BlockRenderLayerMap.INSTANCE.putBlock(PowerRailBlock.POWERRAIL1, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(PowerRailBlock.POWERRAIL2, RenderLayer.getCutout());
