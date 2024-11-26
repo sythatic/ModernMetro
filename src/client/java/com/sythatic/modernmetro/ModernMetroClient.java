1a39ce8b86aba5eb85661625571a5ca0200201be
Dom Panaia <dompanaia@gmail.com>
2024-11-26 16:08:16 -0500

    revert

--- src/client/java/com/sythatic/modernmetro/ModernMetroClient.java
+++ src/client/java/com/sythatic/modernmetro/ModernMetroClient.java
@@ -4,19 +4,11 @@ import com.sythatic.modernmetro.block.*;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.util.Identifier;
-import net.minecraft.text.Text;
 
 public class ModernMetroClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        FabricLoader.getInstance().getModContainer("modernmetro").ifPresent(container -> {
-            ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of("shader-support", "shader-support"), container, Text.literal("Shader Support"), ResourcePackActivationType.NORMAL);
-        });
         BlockRenderLayerMap.INSTANCE.putBlock(PowerRailBlock.POWERRAIL1, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(PowerRailBlock.POWERRAIL2, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(PowerRailBlock.POWERRAIL3, RenderLayer.getCutout());
